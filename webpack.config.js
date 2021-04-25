5905375bf24493054501672d6159a40f6d178fd3
Joan J <joanne.javillo@gmail.com>
2021-04-26 00:27:36 +0930

    fixed public icons folder, added dist folder for manifest json, added webpack config js, fixed index html

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  mode: "production",
+  entry: {
+    app: "./public/index.js",
+    db: "/public/db.js"
+  },
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "[name].bundle.js"
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: false,
+      name: "budget-tracker App",
+      short_name: "budget App",
+      description: "An application to track your budget",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      start_url: "/",
+      icons: [{
+        src: path.resolve("public/icons/icon-192x192.png"),
+        sizes: [192,512],
+        destination: path.join("assets", "icons")
+      }]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
+module.exports = config;
